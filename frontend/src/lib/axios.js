a87c4528201623050f2d686f64be7d0b6afd9934
shashibhushan21 <shashi.bkumar21@gmail.com>
2025-04-23 05:04:47 +0530

    fix: downgrade express to 4.18.2 for compatibility; update axios baseURL for development mode; fix whitespace in useAuthStore.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,14 +1,10 @@
 import axios from 'axios';
 
 
-const baseURL = import.meta.env.PROD 
-  ? `${import.meta.env.VITE_API_URL}/api`
-  : '/api';
+const baseURL = import.meta.env.MODE === "development" ? 'http://localhost:3000/api'
+  : '/api'
 
 export const axiosInstance = axios.create({
     baseURL,
     withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import {axiosInstance} from '../lib/axios.js';
 import toast from 'react-hot-toast';
 import { io } from 'socket.io-client';
 
-const BASE_URL = import.meta.env.MODE === " development"? "http://localhost:3000": "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000": "/";
 
 
 export const useAuthStore = create(persist(
